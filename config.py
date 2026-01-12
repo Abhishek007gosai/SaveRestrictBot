3dbddb06358b4e3f87472053183b45eb338d6ebd
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-01-13 04:07:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@
 # Don't Remove Credit 
 # Telegram Channel @RexBots_Official
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8349396319:AAG949JOiZt7fpG-tGlRk7v7pKAbXM2XL80")
 API_ID = int(os.environ.get("API_ID", "29245477"))
 API_HASH = os.environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "8226767954").split(",")]
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://marinkitagawabot:MScnCVJo6yh3XSQe@marin.10zdv1r.mongodb.net/?appName=marin")
 DB_NAME = os.environ.get("DB_NAME", "marin")
 LOG_CHANNEL = -1002456565415
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
